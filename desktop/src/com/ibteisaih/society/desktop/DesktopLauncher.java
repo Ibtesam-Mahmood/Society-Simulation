00d7c786651ecea097e1a37244c946d3739d4331
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2018-12-14 01:04:45 -0500

    code-init

---
+++ desktop/src/com/ibteisaih/society/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ibteisaih.society.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ibteisaih.society.Simulation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Simulation(), config);
+	}
+}
